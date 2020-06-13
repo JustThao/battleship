a45977b32ec1324e646d2e6bd7a70478f835e422
Trung Pham <phamngocthanhtrung@web.de>
2020-06-13 20:32:51 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def printBoard(boardArr):
+    # print the board
+    pass
+
+def board():
+    # returns the current state of the board
+    pass
+
+# gets x y coordinates from user input and prints the board after
+def guess():
+    pass
+
+def game():
+    pass
+
